8fcf9aef4d16892757aa54276c0b80eaf8a3998d
CEYLON FUCKER <138351304+Cocdas@users.noreply.github.com>
2025-04-15 03:13:17 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "GZ1E3baK#-ZwJ-rWDAWbwJh9pDE29_p0qB149Ntd0Z6g9uyzHEv8",
+SESSION_ID: process.env.SESSION_ID || "nYJHXAxa#8pg_TxJnSzSF1toDokFuhYvpEjnVK3lRWifa9eIaETw",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
