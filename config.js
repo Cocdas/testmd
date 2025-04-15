799a83dd8c59ad96bb655605f4bccc05fc9f48da
CEYLON FUCKER <138351304+Cocdas@users.noreply.github.com>
2025-04-15 04:05:37 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "nYJHXAxa#8pg_TxJnSzSF1toDokFuhYvpEjnVK3lRWifa9eIaETw",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
-AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
-AUTO_BIO:"true",
-AUTO_VOICE:"true"
+MONGODB: process.env.MONGODB || "mongodb://mongo:tmhBEDccuyNbqkcFwMETLoGMzreoiLEL@viaduct.proxy.rlwy.net:57919",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/QF3pFFC9/1797.jpg",
+BOT_NAME: process.env.BOT_NAME || "DIZER-MD",
+LANG: process.env.BOT_LANG || 'EN' ,
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 };
