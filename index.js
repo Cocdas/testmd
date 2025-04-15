e9e03d00163a2f508e51595bf1543364bf2f3785
CEYLON FUCKER <138351304+Cocdas@users.noreply.github.com>
2025-04-15 03:51:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,25 +8,16 @@ fetchLatestBaileysVersion,
 Browsers
 } = require('@whiskeysockets/baileys')
 
-
-const l = console.log
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
 const fs = require('fs')
-const ff = require('fluent-ffmpeg')
 const P = require('pino')
 const config = require('./config')
 const qrcode = require('qrcode-terminal')
-const StickersTypes = require('wa-sticker-formatter')
 const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const { fromBuffer } = require('file-type')
-const bodyparser = require('body-parser')
-const { tmpdir } = require('os')
-const Crypto = require('crypto')
-const path = require('path')
-const prefix = config.PREFIX
+const prefix = '.'
 
 const ownerNumber = ['94787351423']
 
@@ -38,17 +29,17 @@ const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
 fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
-console.log("SESSION DOWNLOADED COMPLETED ✅")
+console.log("Session downloaded ✅")
 })})}
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 9090;
+const port = process.env.PORT || 8000;
 
 //=============================================
 
 async function connectToWA() {
-console.log("CONNECTING ANGEL MD 🎭...");
+console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
 
@@ -68,37 +59,19 @@ if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
 connectToWA()
 }
 } else if (connection === 'open') {
-console.log('♻️ INSTALLING PLUGINS FILES PLEASE WAIT... 🪄')
+console.log('😼 Installing... ')
 const path = require('path');
-fs.readdirSync("./plugin/").forEach((plugin) => {
+fs.readdirSync("./plugins/").forEach((plugin) => {
 if (path.extname(plugin).toLowerCase() == ".js") {
-require("./plugin/" + plugin);
+require("./plugins/" + plugin);
 }
 });
-console.log('PLUGINS FILES INSTALL SUCCESSFULLY ✅')
-console.log('ANGEL MD CONNECTED TO WHATSAPP ENJOY ✅')
+console.log('Plugins installed successful ✅')
+console.log('Bot connected to whatsapp ✅')
 
-let up = `*╭──────────────●●►*
-> * Aɴɢᴇʟ Mᴅ Cᴏɴɴᴇᴄᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʏ Tʏᴘᴇ .Mᴇɴᴜ Tᴏ Gᴇᴛ Cᴏᴍᴍᴀɴᴅ Lɪsᴛ Cʀᴇᴀᴛᴇᴅ Bʏ ᴍʀ ᴀɴᴜʜᴀꜱ*
+let up = `Wa-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-> *Jᴏɪɴ Oᴜʀ Wʜᴀᴛsᴀᴘᴘ Cʜᴀɴɴᴇʟ Fᴏʀ Uᴘᴅᴀᴛᴇs Oꜰ ᴀɴɢᴇʟ ᴍᴅ*
-
-*https://whatsapp.com/channel/0029VatU6wh9cDDhlsnnLh15
-
-> *Fᴏʟʟᴏᴡ Uꜱ Oɴ ᴛɪᴋ ᴛᴏᴋ ᴄʜᴀɴᴇʟ*
-
-*tiktok.com/@sudhu_doni_x
-
-*╭⊱✫ SOBIA MD ✫⊱╮*
-*│✫➠ - 📂REPOSITORY NAME:* *ANGEL MD*
-*│✫➠ - 📃DESCRIPTION:* *SRI LANKA BEST BOT*
-*│✫➠ - 🛡️OWNER:* *ANGEL MD*
-*│✫➠ - 🌐URL:* *https://github.com/mr-anuhas/ANGEL-MD-V1.git*
-
-*YOUR BOT ACTIVE NOW ENJOY♥️🪄*\n\n*PREFIX: ${prefix}*
-
-*╰──────────────●●►*`;
-conn.sendMessage(conn.user.id, { image: { url: `https://files.catbox.moe/ak5sns.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
 
 }
 })
@@ -108,9 +81,7 @@ conn.ev.on('messages.upsert', async(mek) => {
 mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-await conn.readMessages([mek.key])
-}
+if (mek.key && mek.key.remoteJid === 'status@broadcast') return
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
 const content = JSON.stringify(mek.message)
@@ -135,11 +106,10 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
-const isReact = m.message.reactionMessage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
-        
+
 conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
               let mime = '';
               let res = await axios.head(url)
@@ -161,53 +131,8 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
                 return conn.sendMessage(jid, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
               }
             }
-//================ownerreact
-if(senderNumber.includes("94787351423")){
-if(isReact) return
-m.react("👑")
-}
-
-if(senderNumber.includes("94787351423")){
-if(isReact) return
-m.react("👑")
-}
-
-if(senderNumber.includes("94787351423")){
-if(isReact) return
-m.react("🦋")
-   }
-//==========================public react===============//
-// Auto React 
-if (!isReact && senderNumber !== botNumber) {
-    if (config.AUTO_REACT === 'false') {
-        const reactions = [''];
-
-        const randomReaction = reactions[Math.floor(Math.random() * reactions.length)]; // 
-        m.react(randomReaction);
-    }
-}
-
-// Owner React
-if (!isReact && senderNumber === botNumber) {
-    if (config.OWNER_REACT === 'false') {
-        const reactions = [''];
-        const randomOwnerReaction = reactions[Math.floor(Math.random() * reactions.length)]; // 
-        m.react(randomOwnerReaction);
-    }
-}
-//============================        
-        
-//=================================WORKTYPE=========================================== 
-if(!isOwner && config.MODE === "private") return
-if(!isOwner && isGroup && config.MODE === "inbox") return
-if(!isOwner && isGroup && config.MODE === "groups") return
-//======================================================
 
 
-
-
-
-        
 const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
 if (isCmd) {
@@ -238,13 +163,14 @@ mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
+//============================================================================ 
 
 })
 }
 app.get("/", (req, res) => {
-res.send("HEY, ANGEL MD WILL BE STARTED ✅");
+res.send("hey, bot started✅");
 });
-app.listen(port, () => console.log(`Um4d Server listening on port http://localhost:${port}`));
+app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
 setTimeout(() => {
 connectToWA()
-}, 4000);
+}, 4000);  
